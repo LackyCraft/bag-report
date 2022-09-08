92a37e2df1b0aa252d4f0d09731fbc0e8b412846
Даниил Бирюков <zqxw1245@mail.ru>
2022-09-08 17:12:23 +0300

    Добавьте файлы проекта.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        try
+        {
+            Console.WriteLine("Making API Call...");
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                client.BaseAddress = new Uri("https://api.stackexchange.com/2.2/");
+                HttpResponseMessage response = client.GetAsync("answers?order=desc&sort=activity&site=stackoverflow").Result;
+                response.EnsureSuccessStatusCode();
+                string result = response.Content.ReadAsStringAsync().Result;
+            }
+
+        }
+        catch
+        {
+            DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+	}
+}
+
